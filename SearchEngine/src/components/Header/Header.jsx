12dc76c245596682421be6830c70ff9821cee466
priyanka batheja <priyankabatheja21@gmail.com>
2024-04-06 23:33:28 +1100

    header added

---
+++ SearchEngine/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <>
+    <h1>Google Books</h1>
+    <p>Serach through the Google books database and find your favourite read.</p>
+    </>
+  )
+}
+
+export default Header
