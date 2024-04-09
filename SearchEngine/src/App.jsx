e568850d193327d9c2c48296563c7602a54136f5
priyanka batheja <priyankabatheja21@gmail.com>
2024-04-10 08:53:57 +1000

    Form updated

--- SearchEngine/src/App.jsx
+++ SearchEngine/src/App.jsx
@@ -1,11 +1,18 @@
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
+import { useState } from "react";
 
 const App = () => {
+const handleSubmit=()=>{
+  console.log("button clicked")
+}
+
+
   return (
   <>
     <Header/>
-    <Form/>
+    <Form onSubmit={handleSubmit}/>
+    
     </>
   )
 }
---
+++ SearchEngine/src/components/BooksList/BooksList.jsx
@@ -0,0 +1,10 @@
+import { fetchBooks } from "../services/FetchBooks";
+import styles from "./BooksList.mdule.scss"
+
+
+const BookList = ({searchTerm,emptySearch})=>{
+  return(
+    <div >
+    </div>
+  )
+}
